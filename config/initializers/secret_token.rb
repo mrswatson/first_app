02581122922682af112483381ffc4a2ee454ffd1
Drew Watson <my.floating.opera@gmail.com>
2013-01-21 22:42:30 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f67b2acc34952d19fdc5fd59e09164cbb2efaf328d57db60fe7b6a8acea8efd6bd63f9da05ca971ab872f201f89b88861ffaa9f290c54ac447b10018ecae6b60'
